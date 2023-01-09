7344e32009b962026f3ef249bd67df3df349ae3c
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2023-01-09 21:33:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Windows.System.Display;
+
+namespace RequestDisplayOn
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            DisplayRequest displayRequest = new DisplayRequest();
+
+            displayRequest.RequestActive();
+            Console.WriteLine("ディスプレイへの表示要求をアクティブ化しました。");
+            Console.WriteLine("続けるにはEnterキーを押してください。");
+            Console.ReadLine();
+
+            displayRequest.RequestRelease();
+            Console.WriteLine("ディスプレイへの表示要求をリリースしました。");
+            Console.WriteLine("続けるにはEnterキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}
